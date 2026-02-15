d0f285652c558f4d3d5afa0d9312582434e51dda
Jashwanth12707 <a.jashwanth0111@gmail.com>
2026-02-15 14:57:34 +0530

    password resest option added

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,22 +1,38 @@
-from flaskblog import db,login_manager
 from datetime import datetime
+from itsdangerous import URLSafeTimedSerializer
+from flaskblog import db, login_manager
 from flask_login import UserMixin
+from flask import current_app
+
 
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
-class User(db.Model,UserMixin):
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
-
-    # ONE user -> MANY posts
     posts = db.relationship('Post', backref='author', lazy=True)
 
+    def get_reset_token(self, expires_sec=1800):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        return s.dumps({'user_id': self.id})
+
+    @staticmethod
+    def verify_reset_token(token, expires_sec=1800):
+        s = URLSafeTimedSerializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token, max_age=expires_sec)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
     def __repr__(self):
-        return f"User('{self.username}', '{self.email}')"
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
 
 class Post(db.Model):
@@ -24,6 +40,7 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
-
-    # Foreign key → User table
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
